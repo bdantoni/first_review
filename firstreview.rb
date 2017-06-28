60ea37d401d607fd0f79c610fd3bc043fc258541
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-06-28 11:22:45 -0400

    first review question.

---
+++ firstreview.rb
@@ -0,0 +1,25 @@
+def choose_number(x)
+
+random_numbers=[0,1,2,3,4,5,6,7,8,9].sample
+
+
+	if x = random_numbers
+	p "You win!"
+	elsif x > random_numbers
+	p "Too high!"
+	elsif x < random_numbers
+	p "Too low!" 
+	end
+
+
+puts "Your number is #{x}"
+
+end
+
+choose_number(3)
+
+
+
+
+
+
